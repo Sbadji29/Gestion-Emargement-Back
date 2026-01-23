cc2b2063126857236074b28e317da1dc5e8a9858
Mass642464 <massambadiouf269@gmail.com>
2026-01-23 14:46:53 +0000

    liste de surveillant

--- models/sessionExamen.model.js
+++ models/sessionExamen.model.js
@@ -7,14 +7,12 @@ const SessionExamen = {
         id INT AUTO_INCREMENT PRIMARY KEY,
         idExamen INT,
         idSalle INT,
-        idSurveillant INT,
         heureDebut DATETIME,
         heureFin DATETIME,
         nombreInscrits INT,
         nombrePresents INT,
         FOREIGN KEY (idExamen) REFERENCES examen(id),
-        FOREIGN KEY (idSalle) REFERENCES salle(id),
-        FOREIGN KEY (idSurveillant) REFERENCES surveillant(id)
+        FOREIGN KEY (idSalle) REFERENCES salle(id)
       )
     `;
     return db.promise().query(sql);
